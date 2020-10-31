2ef2b652e612d262194fb9530ace0c9d14b30f60
zhuangfanhz <72437452+zhuangfanhz@users.noreply.github.com>
2020-11-01 02:18:57 +0800

    AddFile

---
+++ assignment1/readMoMA.py
@@ -0,0 +1,62 @@
+import csv
+import requests
+from PIL import Image
+from io import BytesIO
+
+class ArtMoMA:
+    def __init__(self, id, width, depth, height, imageUrl, artist):
+        self.id = id
+        self.width = width
+        self.depth = depth
+        self.height = height
+        self.imageUrl = imageUrl
+        self.artist = artist
+        self.imagePath = ''
+
+        if self.width:
+            self.width = float(self.width) * 10
+        else:
+            self.width = 10
+
+        if self.height:
+            self.height = float(self.height) * 10
+        else:
+            self.height = 10
+
+        if self.depth:
+            self.depth = float(self.depth) * 10
+        else:
+            self.depth = 10
+
+    def describe(self):
+        print("artist:" + self.artist, "id:" + self.id, "width:" + str(self.width), "depth:" + str(self.depth), "height:" + str(self.height))
+
+    def getImageFile(self):
+        if self.imageUrl:
+            response = requests.get(self.imageUrl)
+            try:
+                im = Image.open(BytesIO(response.content))
+            except OSError:
+                return None
+            path = "Path to where you want to save the image (Add a folder ArtImages first!!!)"
+            self.imagePath = path
+            im.save(path, "JPEG")
+
+artPieces = []
+with open("Path to where the Arworks.csv file is located", encoding = 'utf-8-sig') as artFile:
+    artReader = csv.DictReader(artFile)
+
+    for row in artReader:
+        id = row['ObjectID']
+        width = row['Width (cm)']
+        height = row['Height (cm)']
+        depth = row['Depth (cm)']
+        imageUrl = row['ThumbnailURL']
+        artist = row['Artist']
+        if width or depth or height:
+            artPiece = ArtMoMA(id, width, depth, height, imageUrl, artist)
+            artPieces.append(artPiece)
+
+for art in artPieces:
+    if "Pollock" in art.artist:
+        art.getImageFile()
---
+++ assignment1/readTATE.py
@@ -0,0 +1,30 @@
+# Imports:
+# use python --version to check what version of python your standard installation is
+# if this is 2.x you will have to use pip3 install otherwise pip install
+
+import csv
+import requests
+# pip install pillow
+from PIL import Image
+from io import BytesIO
+
+# Define the ArtTate class, with all attributes that you find usefull
+class ArtTate:
+    # Define the initialise function accordingly
+    def __init__(self):
+        # delete pass when you start editing, this is a placeholder keyword to say that nothing happens
+        pass
+
+    # Define a function that prints a description
+    def describe(self):
+        # delete pass when you start editing, this is a placeholder keyword to say that nothing happens
+        pass
+
+    # implement the get image function that saves the image to the specified folder
+    def getImageFile(self):
+        # delete pass when you start editing, this is a placeholder keyword to say that nothing happens
+        pass
+
+# Read in the rows of the artwork_data.csv file into a list of ArtTate objects
+
+# write a loop that saves all artwork thumbnails of an artist to a specific folder
